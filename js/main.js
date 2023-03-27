25dfd166ec6993c31f7cc24fde0c9197bf5b3b94
Rencist <dafianakhdan@gmail.com>
2023-03-27 15:51:06 +0700

    feat: news

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// slider
+$('.carousel').carousel({
+  interval: 2000,
+  pause: 'hover',
+  wrap: true,
+  keyboard: true,
+});
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 40) {
+      $('#topBtn').fadeIn();
+    } else {
+      $('#topBtn').fadeOut();
+    }
+  });
+  $('#topBtn').click(function () {
+    $('html,body').animate({ scrollTop: 0 }, 800);
+  });
+});
